c196b60565130126235a3e6d5305ffddd95a1cf2
Joroovb <jorisvanbreugel@mailbox.org>
2022-06-02 09:52:02 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+function add(num1, num2) {
+    return 0;
+}
+
+function subtract(num1, num2) {
+    return 0;
+}
+
+function multiply(num1, num2) {
+    return 0;
+}
+
+function divide(num1, num2) {
+    return 0;
+}
+
+function power(num1, num2) {
+    return 0;
+}
+
+module.exports = {
+    add,
+    subtract,
+    multiply,
+    divide,
+    power
+};
---
+++ index.test.js
@@ -0,0 +1,21 @@
+const {add, subtract, multiply, divide, power} = require('./index');
+
+test('adds two numbers', () => {
+    expect(add(1, 2)).toBe(0);
+});
+
+test('subtracts two numbers', () => {
+    expect(subtract(1, 2)).toBe(0);
+});
+
+test('multiplies to numbers', () => {
+    expect(multiply(1, 2)).toBe(0);
+});
+
+test('divides two numbers', () => {
+    expect(divide(1, 2)).toBe(0);
+});
+
+test('powers to numbers', () => {
+    expect(power(1, 2)).toBe(0);
+});
